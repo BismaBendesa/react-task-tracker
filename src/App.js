e2c0317694d2199240843b29ce871ae3027a6539
Bisma Bendesa <bismabendesajaya@gmail.com>
2022-02-21 15:49:11 +0800

    Import the message component and create the delete task function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState } from "react";
+import Message from "./components/Message";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -25,10 +26,19 @@ function App() {
     },
   ]);
 
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        <Message type="warning" text="No task to show" />
+      )}
     </div>
   );
 }
