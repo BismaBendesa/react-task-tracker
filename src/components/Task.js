b70e81f859598c1d0d6b17f389b04698eec3ce67
Bisma Bendesa <bismabendesajaya@gmail.com>
2022-02-21 16:14:34 +0800

    Set the border to green if the reminder is true

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,10 @@ import { FaTimes } from "react-icons/fa";
 const Task = ({ task, onDelete, toggleReminder }) => {
   return (
     <>
-      <div className="task" onDoubleClick={() => toggleReminder(task.id)}>
+      <div
+        className={`task ${task.reminder ? "reminder" : ""}`}
+        onDoubleClick={() => toggleReminder(task.id)}
+      >
         <h3>
           {task.text}{" "}
           <FaTimes
